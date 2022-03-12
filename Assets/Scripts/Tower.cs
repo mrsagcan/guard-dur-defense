272dfce3769fef1575069cecbe3516dccfed8f65
Ahmet Emre Sağcan <mrsagcan@yahoo.com>
2022-03-12 16:54:21 +0300

    Towers don't shoot enemies if it's not reachable

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,16 +1,44 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] private Transform targetEnemy;
-    
+    [SerializeField] private float attackRange;
+    private ParticleSystem projectile;
+    private void Start()
+    {
+        projectile = GetComponentInChildren<ParticleSystem>();
+    }
+
     private void Update()
     {
-        LookAtEnemy();
+        if (targetEnemy)
+        {
+            LookAtEnemy();
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.position, gameObject.transform.position);
+        Shoot(distanceToEnemy <= attackRange);
     }
 
     private void LookAtEnemy()
     {
         transform.LookAt(targetEnemy);
+    }
+
+    private void Shoot(bool state)
+    {
+        var emissionModule = projectile.emission;
+        emissionModule.enabled = state;
     }
 }
