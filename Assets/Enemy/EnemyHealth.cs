90438ae6257b6fbda1f3cd94125021ef4b79c409
mrsagcan <mrsagcan@yahoo.com>
2023-02-15 14:37:26 +0300

    Difficulty ramping for enemies

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int maxHitPoints = 5;
+    
+    [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
+    [SerializeField] private int difficultyRamp = 1;
     private int currentHitPoints = 0;
 
     private Enemy enemy;
@@ -30,6 +34,7 @@ public class EnemyHealth : MonoBehaviour
         currentHitPoints--;
         if (currentHitPoints <= 0)
         {
+            maxHitPoints += difficultyRamp;
             gameObject.SetActive(false);
             enemy.RewardGold();
         }
