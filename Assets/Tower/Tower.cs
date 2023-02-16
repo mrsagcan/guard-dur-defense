4eb1b94cab6f85af149743aea8f71d14f6f039d4
mrsagcan <mrsagcan@yahoo.com>
2023-02-17 01:04:18 +0300

    Build timer for towers

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -7,9 +7,24 @@ using UnityEngine.UIElements;
 public class Tower : MonoBehaviour
 {
     [SerializeField] private int cost = 75;
+    [SerializeField] private float buildDelay = 1f;
     
     private Bank bank;
-    
+
+    private void Start()
+    {
+        TurnOffChildren();
+        StartCoroutine(Build());
+    }
+
+    private void TurnOffChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -25,5 +40,18 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
